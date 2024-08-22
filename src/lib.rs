af3c115c3de5d8c3eb272a8888c5344a0fb861b0
sotatek <sotatek@sotateks-MacBook-Pro-43.local>
2024-08-22 18:41:53 +0700

    1.Add Shell Scripts fox SQLx
    2.Generate migration files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
 use std::net::TcpListener;
 
 use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok()
+}
+
+// Let's start simple: we always return a 200 OK
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    println!("{}", _form.name);
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
@@ -13,6 +26,7 @@ pub fn run(listener: TcpListener) -> std::io::Result<Server> {
             //.route("/", web::get().to(greet))
             //.route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
